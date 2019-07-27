a73413f0e5d2dfb168ba2abe26d15a2aaf698014
Iven <ivenkoethke@hotmail.de>
2019-07-27 19:04:18 +0200

    Created front component, started styling

---
+++ src/FrontComponent/Front.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Front.css'
+
+function Front(props) {
+    return (
+        <div className="front">
+            <div className="front-header-line">
+                <h1 className="front-header-main">I AM IVEN KÖTHKE</h1><span className="blinking-cursor front-header-main">|</span>
+            </div>
+            <h1 className="front-header-background">blue</h1>
+        </div>
+        );
+}
+
+export default Front;
