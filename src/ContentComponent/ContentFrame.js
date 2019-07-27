e75a71475456b32269f0571d4025bd91dfdd5861
Iven <ivenkoethke@hotmail.de>
2019-07-27 05:55:40 +0200

    Implemented stub for contentframe component to hold core content. Added matching css

---
+++ src/ContentComponent/ContentFrame.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ContentFrame.css'
+
+class ContentFrame extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: props.mode
+        };
+    }
+
+    render() {
+        return (
+            <div className="content-frame">
+                Great content coming !
+                Currently displaying: {this.state.mode}
+            </div>
+        );
+    }
+}
