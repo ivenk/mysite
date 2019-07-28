0bd6af9f681fa85f0ac3833f05e60410f90ae44d
Iven <ivenkoethke@hotmail.de>
2019-07-28 06:22:26 +0200

    Created logic to have one selected button in navbar with different look. Switches once another button is pressed

--- src/NavigationBarComponent/NavBar.js
+++ src/NavigationBarComponent/NavBar.js
@@ -2,30 +2,49 @@ import React from 'react';
 import './NavBar.css';
 
 class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked: "",
+        };
+    }
+
     render() {
         return (
             <div className="nav-bar">
                 <ul className="nav-bar-list">
-                    <li>{this.renderButton("Home")}</li>
-                    <li>{this.renderButton("Contact")}</li>
+                    <li id="home-button" className="nav-bar-list-button">{this.renderButton("Home", (this.state.clicked === "Home"))}</li>
+                    <li id="contact-button" className="nav-bar-list-button">{this.renderButton("Contact", (this.state.clicked === "Contact"))}</li>
                 </ul>
             </div>
         );
     }
 
-    renderButton(name) {
+    renderButton(name, isClicked) {
         return(
             <NavBarButton
                 name={name}
-                onClick={() => alert(name)}
+                onClick={() => this.handleClick(name)}
+                isClicked = {isClicked}
             />
         );
+    }
+
+    handleClick(buttonName) {
+        this.setState({
+            clicked: buttonName,
+        });
     }
 }
 
 function NavBarButton(props) {
+    let classes = "nav-bar-button";
+    if(props.isClicked) {
+        classes += " nav-bar-button-active"
+    }
+
     return (
-        <button className="nav-bar-button" onClick={props.onClick}>
+    <button className={classes}  onClick={props.onClick}>
             {props.name}
         </button>
     );
