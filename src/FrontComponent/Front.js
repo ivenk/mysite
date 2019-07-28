a361d8a0c8fe6de93cbbb0d7169152d244876ff1
Iven <ivenkoethke@hotmail.de>
2019-07-28 17:29:06 +0200

    Added navbar above typewriter on front page

--- src/FrontComponent/Front.js
+++ src/FrontComponent/Front.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Front.css'
 import Typewriter from "./Typewriter";
+import NavBar from "../NavigationBarComponent/NavBar";
 
 function Front(props) {
     return (
         <div className="front">
+            <NavBar />
             <Typewriter text="IVEN KÖTHKE" speed="150" static="I AM " typingDelay="2000"/>
             {/*<h1 className="front-header-background">blue</h1> */}
             <i className="material-icons md-96 absolute_center_bottom">expand_more</i>
