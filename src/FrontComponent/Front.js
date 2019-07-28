c614d8d9bca60e8acd301549548fb781df0d8e80
Iven <ivenkoethke@hotmail.de>
2019-07-28 05:30:43 +0200

    Created typewriter class and styling and added it to front

--- src/FrontComponent/Front.js
+++ src/FrontComponent/Front.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Front.css'
+import Typewriter from "./Typewriter";
 
 function Front(props) {
     return (
         <div className="front">
-            <div className="front-header-line">
-                <h1 className="front-header-main">I AM IVEN KÖTHKE</h1>
-            </div>
-            <h1 className="front-header-background">blue</h1>
+            <Typewriter text="IVEN KÖTHKE" speed="150" static="I AM " typingDelay="2000"/>
+            {/*<h1 className="front-header-background">blue</h1> */}
+            <i className="material-icons md-96 absolute_center_bottom">expand_more</i>
         </div>
-        );
+    );
 }
 
 export default Front;
---
+++ src/FrontComponent/Typewriter.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Typewriter.css'
+
+/* Types the specified code out on the webpage */
+export default class Typewriter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: props.text,
+            speed: props.speed,
+            displayText: "",
+            static: props.static,
+            typingDelay: props.typingDelay,
+        };
+    }
+
+    componentDidMount() {
+        this.startTyping();
+    }
+
+    startTyping() {
+        console.log("Started typing ...");
+        setTimeout(() => {
+            this.type();
+        }, this.state.typingDelay);
+    }
+
+    type(){
+        let displayText = this.state.displayText;
+        let text  = this.state.text;
+
+        if(text.length > displayText.length) {
+            displayText = text.substring(0, displayText.length+1)
+            this.setState({
+                displayText: displayText,
+            });
+            setTimeout(() => {
+                this.type();
+            }, this.state.speed);
+        } else {
+            console.log("Finished typing !");
+        }
+    }
+
+    render() {
+        return(
+            <div className="type-writer front-header-main">
+                <span className="type-writer-static">{this.state.static}</span>
+                <span className="type-writer-typed">{this.state.displayText}</span>
+            </div>
+        )
+    }
+
+
+
+}
