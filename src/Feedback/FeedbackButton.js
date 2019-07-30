2327b42f2d6c165c6f57b859b5dd3487f24c79ec
Iven <ivenkoethke@hotmail.de>
2019-07-30 23:40:46 +0200

    Created Feedback section with feedback button

---
+++ src/Feedback/FeedbackButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FeedbackButton.css'
+
+class FeedbackButton extends React.Component {
+    render() {
+        return (
+                <button className="feedback-button">
+                    Feedback
+                </button>
+        );
+    }
+}
