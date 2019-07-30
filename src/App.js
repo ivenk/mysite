d53ee399366e96fc153164081d7ec40f3e831a49
Iven <ivenkoethke@hotmail.de>
2019-07-30 23:40:00 +0200

    Removed unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import NavBar from "./NavigationBarComponent/NavBar";
 import ContentFrame from "./ContentComponent/ContentFrame"
 import Front from "./FrontComponent/Front"
 
